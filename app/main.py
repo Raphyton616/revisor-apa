1c98d22147e8b1df7a95499ce648f10e65af9fd0
Raphyton616 <sebadarkk008@gmail.com>
2026-09-19 19:46:57 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,7 +29,10 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
 
 
 @app.post("/api/analyze")
